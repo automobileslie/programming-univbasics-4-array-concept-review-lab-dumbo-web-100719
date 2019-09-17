27340a4ddc63cf1f1b9b741b44477f582433b5af
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-09-17 19:55:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,8 +11,13 @@ end
 end
 
 def find_max_value(array)
-  array.sort!
-  array.last
+ max = array[0]
+ counter = 1
+ while counter < array.length do 
+   if array[counter] > max
+     max= array[counter]
+ 
+ end
 end
 
 def find_min_value(array)
